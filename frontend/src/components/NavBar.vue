a8ab198d7ec6ec4e57a4990bc4f6bb843cce42b2
Xierip <xierip@gmail.com>
2019-03-31 23:00:54 +0200

    NavBar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,22 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="">TODO</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="https://getbootstrap.com/docs/4.3/examples/navbar-fixed/#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://getbootstrap.com/docs/4.3/examples/navbar-fixed/#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="https://getbootstrap.com/docs/4.3/examples/navbar-fixed/#">Disabled</a>
-        </li>
-      </ul>
-    </div>
   </nav>
 </template>
 
