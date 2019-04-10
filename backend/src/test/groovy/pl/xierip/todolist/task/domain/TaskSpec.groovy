d46c86126adf8c73a6cef58af5a225800b636ecd
Xierip <xierip@gmail.com>
2019-04-10 17:01:42 +0200

    groovy fix in gradle + test

--- backend/src/test/groovy/pl/xierip/todolist/task/domain/TaskSpec.groovy
+++ backend/src/test/groovy/pl/xierip/todolist/task/domain/TaskSpec.groovy
@@ -4,6 +4,7 @@ package pl.xierip.todolist.task.domain
 import pl.xierip.todolist.todolist.task.domain.TaskConfiguration
 import pl.xierip.todolist.todolist.task.domain.TaskFacade
 import pl.xierip.todolist.todolist.task.dto.TaskDTO
+import pl.xierip.todolist.todolist.task.dto.exceptions.TaskNotFoundException
 import spock.lang.Specification
 
 class TaskSpec extends Specification implements SampleTasks {
@@ -14,5 +15,12 @@ class TaskSpec extends Specification implements SampleTasks {
             TaskDTO newCoffee = taskFacade.createTask(makeCoffee)
         expect: "system return task"
             taskFacade.findById(newCoffee.id) == newCoffee
+    }
+
+    def "should throw exception when ask for task that's not in system"() {
+        when: "task id is invalid"
+        taskFacade.findById(-1)
+        then:
+        thrown(TaskNotFoundException)
     }
 }
